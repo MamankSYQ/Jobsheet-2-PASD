c0d3e455ff37dd4574ecc3e13e3dedf7691b4f62
MamankSYQ <abdurrahmansyafiq151123@gmail.com>
2026-02-18 09:59:38 +0700

    Praktikum 2 revisi

--- Jobsheet 2 PASD/MahasiswaMain01.java
+++ Jobsheet 2 PASD/MahasiswaMain01.java
@@ -1,14 +1,14 @@
 public class MahasiswaMain01 {
     public static void main(String[] args) {
-        Mahasiswa01 mhsl = new Mahasiswa01();
-        mhsl.nama = "Muhammad Ali Farhan";
-        mhsl.nim = "2241720171";
-        mhsl.kelas = "SI 2J";
-        mhsl.ipk = 3.55;
+        Mahasiswa01 mhs1 = new Mahasiswa01();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
 
-        mhsl.tampilkanInformasi();
-        mhsl.ubahKelas("SI 2K");
-        mhsl.updateIpk(3.60);
-        mhsl.tampilkanInformasi();
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
     }
 }
